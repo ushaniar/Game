6370c7ff0610d56cf93b3a422c4d2cd65b895f49
ushaniar <aushani@umassd.edu>
2015-06-24 19:39:26 -0400

    new file:   Class1.cs 	new file:   GameObjects.cs

---
+++ Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApplication1 {
+
+    public class LevelOne : Canvas {
+        public SpaceShip myPlayer;
+        public Ball myBall;
+
+        private Vitamin[,] vArray = new Vitamin[5, 10];
+
+        // Add Vitamins to Level
+        private void addVitamins() {
+            double vWidth = this.Width / vArray.GetLength(0);
+            double vHeight = this.Height / vArray.GetLength(1);
+
+            for (int i = 0; i < vArray.GetLength(0); i++) {
+                for (int k = 0; k < vArray.GetLength(1); k++) {
+                    // Needs fixing
+                    vArray[i, k] = new Vitamin(new Rect(i * vWidth, k * vHeight * .4 + 50, vWidth - 1, 100 / vHeight));
+                    this.Children.Add(vArray[i, k]);
+                }
+            }
+        }
+
+        public LevelOne() {   
+            this.Width = 400;
+            this.Height = 300;
+
+            this.Background = Brushes.Beige;
+
+            // Create objects
+            myPlayer = new SpaceShip();
+            myBall = new Ball();
+
+            // Add objects to level
+            this.Children.Add(myBall);
+            this.Children.Add(myPlayer);
+
+            // Add event to move player 
+            this.MouseMove += myPlayer.move;
+
+            // Add vitamins
+            // addVitamins();             
+        }
+    }
+}
---
+++ GameObjects.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Input;
+
+namespace WpfApplication1 {
+
+    public class SpaceShip : Shape {
+        RectangleGeometry mySpaceShipGeometry;
+
+        protected override Geometry DefiningGeometry {
+            get { return mySpaceShipGeometry; }
+        }
+
+        public void move(object sender, MouseEventArgs e) {
+            mySpaceShipGeometry.Rect = new Rect(e.GetPosition(this).X - (25), 300, 50, 25);
+        }
+
+        public SpaceShip() {
+            mySpaceShipGeometry = new RectangleGeometry();
+            mySpaceShipGeometry.Rect = new Rect(100, 300, 50, 25);
+            this.Fill = Brushes.Aqua;
+        }  
+    }
+
+    public class Ball : Shape {
+        EllipseGeometry myBall;
+        Point myPoint;
+
+        protected override Geometry DefiningGeometry {
+            get { return myBall; }
+        }
+
+        public void render(Object sender, EventArgs w) {
+            // Moves the ball
+            myBall.Center = new Point(myPoint.X++, myPoint.Y++);
+        }
+
+        public Ball() {
+            myBall = new EllipseGeometry(myPoint, 20, 20);
+            this.Fill = Brushes.SkyBlue;
+        }
+    }
+
+    public class Vitamin : Shape {
+        RectangleGeometry myVitamin;
+        Random r;
+
+        protected override Geometry DefiningGeometry {
+            get { return myVitamin; }
+        }
+
+        private void setRandomColor() {
+            r = new Random();
+            switch (r.Next(1, 3)) {
+                case 1:
+                    this.Fill = Brushes.Blue;
+                    break;
+                case 2:
+                    this.Fill = Brushes.Green;
+                    break;
+                case 3:
+                    this.Fill = Brushes.Red;
+                    break;
+                default:
+                    this.Fill = Brushes.Black;
+                    break;
+            }
+        }
+
+        public Vitamin(Rect myRect) {
+            myVitamin = new RectangleGeometry();
+            myVitamin.Rect = myRect;
+            setRandomColor();
+        }
+    }
+}
